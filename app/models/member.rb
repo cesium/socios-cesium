b812f9a0742448da895df4caa7c759065d7ec6d4
Beatgodes <cristiano.sousa126@gmail.com>
2013-10-26 00:36:25 +0100

    Added some basic validators

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,6 @@
 class Member < ActiveRecord::Base
-    
+	validates :name, presence: true
+    validates :member_number, presence: true, uniqueness: true
+    validates :email, presence: true, email: true, uniqueness: true
+    validates :phone, phone: true
 end
