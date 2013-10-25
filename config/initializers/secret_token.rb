58dbbbbf70bb4f2c3975d7951b28a2899cf079ac
André Santos <andreccdr@gmail.com>
2013-10-25 20:27:47 +0100

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SociosCesium::Application.config.secret_key_base = 'decdbf26f906d7bae4f339a0736e705a94273130cd2c58b73ec0c151bcecdaddaad95039949707cdf7ecf2db0c57160b69c3132cb3729e0d73f61b8018f206d5'
